534366ca2f12d55d522feda32a272d50eb2547d4
Vincent Billerey <vincent.billerey.web@gmail.com>
2023-08-21 10:56:31 +0200

    Ajout du catalogue de l'étape 4. Refonte du Sass. Etape suivante : la pagination

--- front-page.php
+++ front-page.php
@@ -23,6 +23,39 @@
             <h2 class="container-hero__titre"> Photographe Event </h2>
     </section>
 
+    <section class="container-photo">
+        <!-- Catalogue -->
+        <div class="container-photo-apparente" data-page="1">
+            <?php
+
+            //* Nouvelle instance de WP_Query pour récupérer les 12 premiers posts pour le catalogue photo
+            $pagin = get_query_var('paged') ? get_query_var('paged') : 1;
+            $args_photos = array(
+                'post_type' => 'photos',
+                'posts_per_page' => 12,
+                'orderby' => 'rand',
+                'paged' => $pagin,
+            );
+
+            $catalogue_photos = new WP_Query($args_photos);
+
+            if ($catalogue_photos->have_posts()) {
+                while ($catalogue_photos->have_posts()) {
+                    $catalogue_photos->the_post();
+
+                    // structure du catalogue
+                    get_template_part('template-parts/photo-block');
+                }
+                wp_reset_postdata();
+            }
+            ?>
+        </div>
+
+        <div id="load-more-container">
+            <button id="load-more-btn" class="btn">Charger plus</button>
+        </div>
+
+    </section>
 
     </main>
 
--- template-parts/content-single-photos.php
+++ template-parts/content-single-photos.php
@@ -49,69 +49,104 @@
 
         <?php
             //Flèches précédent et suivant
-            $nextPost = get_next_post();
-            $previousPost = get_previous_post();
+            $next_post = get_next_post();
+            $previous_post = get_previous_post();
 
-            // Si on est sur la dernière photo, définir $nextPost comme le premier post
-            if (empty($nextPost)) {
+            // Si on est sur la dernière photo, définir $next_post comme le premier post
+            if (empty($next_post)) {
                 $args = array(
                     'posts_per_page' => 1,
                     'order'          => 'ASC',
                     'post_type'      => 'photos' // 
                 );
-                $firstPost = get_posts($args);
-                if (!empty($firstPost)) {
-                    $nextPost = $firstPost[0];
+                $first_post = get_posts($args);
+                if (!empty($first_post)) {
+                    $next_post = $first_post[0];
                 }
             }
 
-            // Si on est sur la première photo, définir $previousPost comme le dernier post
-            if (empty($previousPost)) {
+            // Si on est sur la première photo, définir $previous_post comme le dernier post
+            if (empty($previous_post)) {
                 $args = array(
                     'posts_per_page' => 1,
                     'order'          => 'DESC',
                     'post_type'      => 'photos' // 
                 );
-                $lastPost = get_posts($args);
-                if (!empty($lastPost)) {
-                    $previousPost = $lastPost[0];
+                $last_post = get_posts($args);
+                if (!empty($last_post)) {
+                    $previous_post = $last_post[0];
                 }
             }
         ?>
 
         <div class="container-contact__navigation-arrows">
-            <?php if (!empty($previousPost) || !empty($nextPost)) { ?>
+            <?php if (!empty($previous_post) || !empty($next_post)) { ?>
                 <div class="container-miniature">
                     <?php
-                        $thumbnailID = null;
+                        $thumbnail_ID = null;
 
-                        if (!empty($nextPost)) {
-                            $thumbnailID = get_post_thumbnail_id($nextPost->ID);
-                        } elseif (!empty($previousPost)) {
-                            $thumbnailID = get_post_thumbnail_id($previousPost->ID);
+                        if (!empty($next_post)) {
+                            $thumbnail_ID = get_post_thumbnail_id($next_post->ID);
+                        } elseif (!empty($previous_post)) {
+                            $thumbnail_ID = get_post_thumbnail_id($previous_post->ID);
                         }
 
-                        if ($thumbnailID) {
-                            echo wp_get_attachment_image($thumbnailID, 'thumbnail', false, ['class' => 'container-miniature__img-arrows']);
+                        if ($thumbnail_ID) {
+                            echo wp_get_attachment_image($thumbnail_ID, 'thumbnail', false, ['class' => 'container-miniature__img-arrows']);
                         }
                     ?>
                 </div>
                 <div class="container-arrows">
-                    <?php if (!empty($previousPost)) { ?>
-                        <a href="<?php echo get_permalink($previousPost->ID) ?>"><img class="arrowLeft" src="<?php echo get_theme_file_uri() .'/assets/images/arrow-left.png';?>" alt="Flèche précédent"></a>
+                    <?php if (!empty($previous_post)) { ?>
+                        <a href="<?php echo get_permalink($previous_post->ID) ?>"><img class="arrow-left" src="<?php echo get_theme_file_uri() .'/assets/images/arrow-left.png';?>" alt="Flèche précédent"></a>
                     <?php } ?>
-                    <?php if (!empty($nextPost)) { ?>
-                        <a href="<?php echo get_permalink($nextPost->ID) ?>"><img class="arrowRight" src="<?php echo get_theme_file_uri() .'/assets/images/arrow-right.png';?>" alt="Flèche suivant"></a>
+                    <?php if (!empty($next_post)) { ?>
+                        <a href="<?php echo get_permalink($next_post->ID) ?>"><img class="arrow-right" src="<?php echo get_theme_file_uri() .'/assets/images/arrow-right.png';?>" alt="Flèche suivant"></a>
                     <?php } ?>
                 </div>
             <?php } ?>
         </div>
     </div>
 
-    <div class="container-photo-block">
+    <div class="container-photo">
         <p class="container-photo-block__title"> vous aimerez aussi </p>
-        
-        <?php get_template_part('template-parts/photo-block'); ?>
+        <div class="container-photo-apparente">
+            <?php
+                $categories = get_the_terms(get_the_ID(), 'categorie');
+                if ($categories && !is_wp_error($categories)) {
+                    $category_ids = wp_list_pluck($categories, 'term_id');
+
+                    $args = array(
+                        'post_type' => 'photos',
+                        'posts_per_page' => 2,
+                        'orderby' => 'rand',
+                        'post__not_in' => array(get_the_ID()),
+                        'tax_query' => array(
+                            array(
+                                'taxonomy' => 'categorie',
+                                'field' => 'term_id',
+                                'terms' => $category_ids,
+                            ),
+                        ),
+                    );
+
+                    $photos_query = new WP_Query($args);
+
+                    if ($photos_query->have_posts()) {
+                        while ($photos_query->have_posts()) {
+                            $photos_query->the_post();
+                            $photo_retrieval = get_field('photos');
+                            ?>
+                            
+                            <?php get_template_part('template-parts/photo-block'); ?>
+
+                            <?php
+                        }
+                        wp_reset_postdata();
+                    }
+                }
+            ?>
+        </div>
 
         <button type="button" class="container-photo-block__all-btn">Toutes les photos</button>
     </div>
